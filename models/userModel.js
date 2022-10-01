58567d8ba807f8d635d8790af4bc23a101582ce4
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-10-01 23:21:16 +0530

    Encrypting Password in Database

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,12 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, "Please provide a valid email"],
   },
-  photo: String,
+  pic: {
+   type: String,
+   required: true,
+   default:
+    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
   role: {
     type: String,
     enum: ["shark", "sponsor", "pitcher"],
@@ -27,10 +32,30 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
     select: false,
   },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
   comanyName: String,
   comapanyDescription: String,
   companyImage: String,
   companyPosition: String,
+},
+{ timestaps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
