e58caabf874d85b30417f35c0a40647e39eb811b
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-10-01 08:51:15 +0530

    Server: User Model

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['pitcher', 'shark', 'sponsor'],
+    default: 'sponsor'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
