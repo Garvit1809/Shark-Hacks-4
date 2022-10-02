843ae81f45ff964fe79cf59331430770c7ccddc0
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-10-02 08:49:47 +0530

    Chat: ChatLogic and ContextAPI

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
-    <App />
+    <ChatProvider>
+      <App />
+    </ChatProvider>
   </ChakraProvider>
 );
